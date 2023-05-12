745325ed7cf2cef352802727d1caf19fff40a54d
paonep1 <95166898+paonep1@users.noreply.github.com>
2023-05-12 11:23:29 +0530

    Video no - 35 completed (NewsHunter - React App).With  Some Errors in console

---
+++ src/components/News.js
@@ -0,0 +1,136 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export class News extends Component {
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: "general",
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+  capitalizeFirstLetter = (string)=>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: true,
+      page: 1,
+      totalResults: 0
+    };
+    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsHunter`
+  }
+
+  async updateNews() {
+      const newsUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=36638a95296f45828217855d9d80435d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      let newsData = await fetch(newsUrl);
+      let parsedNewsData = await newsData.json();
+      this.setState({
+        articles: parsedNewsData.articles,
+        totalResults: parsedNewsData.totalResults,
+        loading: false
+      });
+  }
+
+  async componentDidMount() {
+    this.updateNews()
+  }
+
+
+  // handlePerClick = async () => {
+  //   this.setState({page: this.state.page - 1})
+  //   this.updateNews();
+  // };
+
+  // handleNextClick = async () => {
+  //   this.setState({page: this.state.page + 1})
+  //   this.updateNews();
+  // };
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const newsUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=36638a95296f45828217855d9d80435d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let newsData = await fetch(newsUrl);
+    let parsedNewsData = await newsData.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedNewsData.articles),
+      totalResults: parsedNewsData.totalResults,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h2 className="text-center my-5">NewsHunter - {this.capitalizeFirstLetter(this.props.category)} News</h2>
+
+
+        {this.state.loading && <Spinner />}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      des={element.description ? element.description : ""}
+                      imgUrl={element.urlToImage}
+                      url={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+
+        {/* <div className="my-4 d-flex justify-content-center">
+          <button
+            onClick={this.handlePerClick}
+            disabled={this.state.page <= 1}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-sm btn-warning mx-2"
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            onClick={this.handleNextClick}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-sm btn-warning"
+          >
+            Next &rarr;
+          </button>
+        </div> */}
+
+      </>
+    );
+  }
+}
+
+export default News;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    let {title, des, imgUrl, url, author, date, source} = this.props;
+    return (
+      <div className='my-3'>
+        <div className="card">
+        <span className="position-absolute top-0 end-0 translate-middle badge rounded-pill bg-danger">{source}</span>
+          <img className="card-img-top" src={!imgUrl?"https://cdn.dribbble.com/users/937082/screenshots/5516643/blob_4x":imgUrl} alt="Card img"/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{des}</p>
+              <p className="card-text"><small className='text-muted'>By {author?author:"Unknown"} on {new Date(date).toGMTString()} </small></p>
+              <a rel="noreferrer" href={url} target='_blank' className="btn btn-sm btn-warning">Read More...</a>
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItem
